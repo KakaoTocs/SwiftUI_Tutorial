4ffd618c73c4adbccd4eb82b8f37a29c40be98a3
KakaoTocs <kimjinu5555@gmail.com>
2019-09-12 19:01:10 +0900

    Landmark Project Init

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jinu Kim on 2019/09/12.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

5f7f6fedae38ddac919a7c2398b05b3915a4d61b
KakaoTocs <kimjinu5555@gmail.com>
2019-09-13 11:17:53 +0900

    Add MapView 랜드마크 위치  지도로 표시

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Jinu Kim on 2019/09/13.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MapView.UIViewType {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -166.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
